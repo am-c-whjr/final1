77b16a324d2768a9651ae3937db0cf4b0689f0d4
am-c-whjr <84730467+am-c-whjr@users.noreply.github.com>
2021-06-28 11:35:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function gotonewyork(){
+    window.location="New_york.html";
+}function gotoworld(){
+    window.location="world.html";
+}
+function logout(){
+    window.location="index.html";
+}
+}function gotosolution(){
+    window.location="solution.html";
+}
